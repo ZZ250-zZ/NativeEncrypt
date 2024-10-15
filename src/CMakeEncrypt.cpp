8334877abd213d8d44f01770cd03d89f60718927
123 <123>
2024-10-15 17:27:23 +0800

    libcurl

--- src/CMakeEncrypt.cpp
+++ src/CMakeEncrypt.cpp
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include "com_me_study_javaCore_jni_NativeEncryptUtils.h"
+#include <curl/curl.h>
 
 using namespace std;
 
@@ -10,9 +11,39 @@ using namespace std;
 // 定义常量值用于按位异或
 const jbyte XOR_VALUE = 19;
 
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+	((std::string*)userp)->append((char*)contents, size * nmemb);
+	return size * nmemb;
+}
+
 //该函数头复制于 com_me_study_javaCore_jni_NativeEncryptUtils.h
 JNIEXPORT jbyteArray JNICALL Java_com_me_study_javaCore_jni_NativeEncryptUtils_decrypt
 (JNIEnv *env, jclass clazz, jbyteArray inputArray) {
+	CURL* curl;
+	CURLcode res;
+	std::string readBuffer;
+
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	curl = curl_easy_init();
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, "https://gitee.com/uahioacnao/tvs/raw/master/key.txt");
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+
+		res = curl_easy_perform(curl);
+		if (res != CURLE_OK) {
+			printf("HTTPS 异常");
+			return nullptr; // 返回 null
+		}
+		else {
+			// 使用 printf 打印 readBuffer 的内容
+			printf("%s\n", readBuffer.c_str());
+		}
+		curl_easy_cleanup(curl);
+	}
+	curl_global_cleanup();
+
 	// 获取输入字节数组的长度
 	jsize length = env->GetArrayLength(inputArray);
 	if (length <= 0) {
